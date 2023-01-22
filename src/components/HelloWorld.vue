d14a93124804d17e6b953148a8e2cbac6e9738e9
A.N Dike <116586816+Anistetus@users.noreply.github.com>
2023-01-22 11:07:57 -0500

    nonsolambda
    endpoint update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://xg23o7wgdk.execute-api.us-east-1.amazonaws.com/default/saveImageToS3'
+const API_ENDPOINT = 'https://edtgrse9k4.execute-api.us-east-2.amazonaws.com/default/nonsolambda11'
 
 export default {
   name: 'app',
